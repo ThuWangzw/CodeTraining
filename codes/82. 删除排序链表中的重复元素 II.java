314956d74bf498de4a34554fe6373f13ade9c3c3
unknown <wangzw834@163.com>
2021-01-28 14:04:01 +0800

    82 dummy node

--- codes/82. 删除排序链表中的重复元素 II.java
+++ codes/82. 删除排序链表中的重复元素 II.java
@@ -1,3 +1,6 @@
+/*
+    本题可以使用dummy node方法。dummy node指的是，当链表的头部会被删除时，在链表头部插入一个不会被删除的dummy node，使得代码更加简洁
+ */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode cur = head;
