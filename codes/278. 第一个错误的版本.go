34f04f916cf650b5e7622db2f5c5fca0047e6ac2
unknown <wangzw834@163.com>
2021-03-03 16:37:50 +0800

    278

---
+++ codes/278. 第一个错误的版本.go
@@ -0,0 +1,13 @@
+func firstBadVersion(n int) int {
+	start := 1
+	end := n
+	for start <= end {
+		mid := (start + end) / 2
+		if isBadVersion(mid) {
+			end = mid - 1
+		} else {
+			start = mid + 1
+		}
+	}
+	return start
+}
