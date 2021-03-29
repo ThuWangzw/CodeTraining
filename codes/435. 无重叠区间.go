c80bf6eeb3655d7e476d4daa03266195baed16e7
ThuWangzw <wangzw834@163.com>
2021-03-29 14:11:47 +0800

    435

---
+++ codes/435. 无重叠区间.go
@@ -0,0 +1,35 @@
+// 先排序，再使用贪心算法
+type Intervals [][]int
+
+func (intervals Intervals) Len() int {
+	return len(intervals)
+}
+func (intervals Intervals) Swap(i int, j int) {
+	intervals[j], intervals[i] = intervals[i], intervals[j]
+}
+func (intervals Intervals) Less(i, j int) bool {
+	return intervals[i][1] < intervals[j][1]
+}
+
+func eraseOverlapIntervals(intervals [][]int) int {
+	sort.Sort(Intervals(intervals))
+	cnt := 0
+	begin := 0
+	lastMin := math.MinInt32
+	n := len(intervals)
+	for {
+		find := false
+		for i:=begin; i<n; i++ {
+			if intervals[i][0] >= lastMin {
+				find = true
+				begin = i
+				lastMin = intervals[i][1]
+				cnt++
+			}
+		}
+		if !find {
+			break
+		}
+	}
+	return n-cnt
+}
