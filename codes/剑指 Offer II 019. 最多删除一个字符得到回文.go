e25981da40236cbbf9a31c0cda5e347e550d5b64
ThuWangzw <wangzw834@163.com>
2022-08-29 10:43:51 +0800

    JZ2.19

---
+++ codes/剑指 Offer II 019. 最多删除一个字符得到回文.go
@@ -0,0 +1,17 @@
+func isPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func validPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return isPalindrome(s[i+1:len(s)-i]) || isPalindrome(s[i:len(s)-i-1])
+		}
+	}
+	return true
+}
