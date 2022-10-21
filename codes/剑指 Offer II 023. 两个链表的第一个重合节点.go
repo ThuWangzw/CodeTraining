2ade286f089406814600fc97df63fb142a54826d
unknown <wangzw834@163.com>
2022-10-21 17:08:58 +0800

    JZ2.23

---
+++ codes/剑指 Offer II 023. 两个链表的第一个重合节点.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	p := headA
+	q := headB
+	for p != q {
+		if p != nil {
+			p = p.Next
+		} else {
+			p = headB
+		}
+
+		if q != nil {
+			q = q.Next
+		} else {
+			q = headA
+		}
+	}
+	return p
+}
